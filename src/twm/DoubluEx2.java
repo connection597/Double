afc4542f1a00150ed7e272e22d641f6f35968df2
connection579 <wlswn4118@naver.com>
2020-10-29 13:19:52 +0900

    더 넒은 범위의 실수값을 표현하기 위한 부동 소수점 방식을 사용한다

---
+++ src/twm/DoubluEx2.java
@@ -0,0 +1,14 @@
+package twm;
+
+public class DoubluEx2 {
+
+	public static void main(String[] args) {
+		double dnum =1;
+		
+		for(int i = 0; i<10000; i++) {
+			dnum = dnum +0.1;
+		}
+		System.out.println(dnum);
+	}
+
+}
